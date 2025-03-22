e6536051371dc27de910990572be2dbc94035256
khushalibegde <134138053+khushalibegde@users.noreply.github.com>
2025-03-22 12:41:25 +0530

    updates screens

---
+++ src/GreetingsScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const GreetingsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Greetings Screen</Text>
+    </View>
+  );
+};
+
+export default GreetingsScreen;
